63174f418b819886454ae9cf37d3f8a1b8443470
Denis Lazarenko <dentus25@gmail.com>
2022-06-29 17:19:58 +0300

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -4,10 +4,6 @@ import styles from './notification.module.css'
 
 const Notification = ({ message }) => <p className={styles.text}>{message}</p>;
 
-Notification.defaulProps = {
-    message: "",
-}
-
 Notification.propTypes = {
     message: PropTypes.string.isRequired,
 }
